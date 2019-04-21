02f2357629d59c956fccfb98f13d692593aa2eda
Junyoung Kim <junyoung2@gmail.com>
2019-04-21 13:55:42 +0900

    Separate prepare data functions

--- chatbot_tutorial.py
+++ chatbot_tutorial.py
@@ -51,6 +51,7 @@ from decoder import LuongAttnDecoderRNN
 from movie_line_process import loadLines, loadConversations, extractSentencePairs
 from voc import loadPrepareData, trimRareWords, normalizeString
 from voc import MIN_COUNT, MAX_LENGTH, PAD_token, SOS_token, EOS_token
+from prepare_data import batch2TrainData
 
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
@@ -107,108 +108,6 @@ for pair in pairs[:10]:
 
 # Trim voc and pairs
 pairs = trimRareWords(voc, pairs, MIN_COUNT)
-
-
-######################################################################
-# Prepare Data for Models
-# -----------------------
-#
-# Although we have put a great deal of effort into preparing and massaging our
-# data into a nice vocabulary object and list of sentence pairs, our models
-# will ultimately expect numerical torch tensors as inputs. One way to
-# prepare the processed data for the models can be found in the `seq2seq
-# translation
-# tutorial <https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html>`__.
-# In that tutorial, we use a batch size of 1, meaning that all we have to
-# do is convert the words in our sentence pairs to their corresponding
-# indexes from the vocabulary and feed this to the models.
-#
-# However, if you’re interested in speeding up training and/or would like
-# to leverage GPU parallelization capabilities, you will need to train
-# with mini-batches.
-#
-# Using mini-batches also means that we must be mindful of the variation
-# of sentence length in our batches. To accomodate sentences of different
-# sizes in the same batch, we will make our batched input tensor of shape
-# *(max_length, batch_size)*, where sentences shorter than the
-# *max_length* are zero padded after an *EOS_token*.
-#
-# If we simply convert our English sentences to tensors by converting
-# words to their indexes(\ ``indexesFromSentence``) and zero-pad, our
-# tensor would have shape *(batch_size, max_length)* and indexing the
-# first dimension would return a full sequence across all time-steps.
-# However, we need to be able to index our batch along time, and across
-# all sequences in the batch. Therefore, we transpose our input batch
-# shape to *(max_length, batch_size)*, so that indexing across the first
-# dimension returns a time step across all sentences in the batch. We
-# handle this transpose implicitly in the ``zeroPadding`` function.
-#
-# .. figure:: /_static/img/chatbot/seq2seq_batches.png
-#    :align: center
-#    :alt: batches
-#
-# The ``inputVar`` function handles the process of converting sentences to
-# tensor, ultimately creating a correctly shaped zero-padded tensor. It
-# also returns a tensor of ``lengths`` for each of the sequences in the
-# batch which will be passed to our decoder later.
-#
-# The ``outputVar`` function performs a similar function to ``inputVar``,
-# but instead of returning a ``lengths`` tensor, it returns a binary mask
-# tensor and a maximum target sentence length. The binary mask tensor has
-# the same shape as the output target tensor, but every element that is a
-# *PAD_token* is 0 and all others are 1.
-#
-# ``batch2TrainData`` simply takes a bunch of pairs and returns the input
-# and target tensors using the aforementioned functions.
-#
-
-def indexesFromSentence(voc, sentence):
-    return [voc.word2index[word] for word in sentence.split(' ')] + [EOS_token]
-
-
-def zeroPadding(l, fillvalue=PAD_token):
-    return list(itertools.zip_longest(*l, fillvalue=fillvalue))
-
-def binaryMatrix(l, value=PAD_token):
-    m = []
-    for i, seq in enumerate(l):
-        m.append([])
-        for token in seq:
-            if token == PAD_token:
-                m[i].append(0)
-            else:
-                m[i].append(1)
-    return m
-
-# Returns padded input sequence tensor and lengths
-def inputVar(l, voc):
-    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
-    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
-    padList = zeroPadding(indexes_batch)
-    padVar = torch.LongTensor(padList)
-    return padVar, lengths
-
-# Returns padded target sequence tensor, padding mask, and max target length
-def outputVar(l, voc):
-    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
-    max_target_len = max([len(indexes) for indexes in indexes_batch])
-    padList = zeroPadding(indexes_batch)
-    mask = binaryMatrix(padList)
-    mask = torch.ByteTensor(mask)
-    padVar = torch.LongTensor(padList)
-    return padVar, mask, max_target_len
-
-# Returns all items for a given batch of pairs
-def batch2TrainData(voc, pair_batch):
-    pair_batch.sort(key=lambda x: len(x[0].split(" ")), reverse=True)
-    input_batch, output_batch = [], []
-    for pair in pair_batch:
-        input_batch.append(pair[0])
-        output_batch.append(pair[1])
-    inp, lengths = inputVar(input_batch, voc)
-    output, mask, max_target_len = outputVar(output_batch, voc)
-    return inp, lengths, output, mask, max_target_len
-
 
 # Example for validation
 small_batch_size = 5
