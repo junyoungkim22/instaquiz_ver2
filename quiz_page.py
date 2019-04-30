1091bfef974e1e5a2be7cf6e664b1d1a6f22f0da
Junyoung Kim <junyoung2@gmail.com>
2019-04-30 22:10:47 +0900

    Implement working quiz solve page

--- quiz_page.py
+++ quiz_page.py
@@ -8,8 +8,10 @@ def input_page():
 
 @app.route('/quiz', methods = ['POST', 'GET'])
 def start_quiz():
-	if request.method == 'POST':
-		quiz_material = request.form['quiz_material']
-		return quiz_material
-	else:
-		return 'GET!'
+    if request.method == 'POST':
+        quiz_material = request.form['quiz_material']
+        qa_pairs = [("How many sides does a square have?", "4"), ("What is the color of a banana?", "yellow"), ("Who created Microsoft?", "Bill Gates")]
+        return render_template('solve_quiz.html', qa_pairs = qa_pairs)
+		#return quiz_material
+    else:
+        return 'GET!'
